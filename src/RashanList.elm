e33a0039fb973c40e485a6b0690e375e7b51cad8
amit-singhs <amitdrive003@gmail.com>
2021-02-13 23:55:23 +0530

    Rashan list created, for grocery list app.

---
+++ src/RashanList.elm
@@ -0,0 +1,55 @@
+module RashanList exposing (..)
+
+import Browser
+import Html exposing (Html, a, div, text)
+import List exposing (..)
+import Main exposing (Model, init, update, view)
+
+
+
+--Main
+
+
+main =
+    Browser.sandbox { init = init, view = view, update = update }
+
+
+
+-- Model
+
+
+type alias Model =
+    { shoppinglist : List String }
+
+
+init : Model
+init =
+    { shoppingList = [ "My", "name", "is", "Amit" ]
+    }
+
+
+
+
+
+type Msg
+    = Additem String
+
+
+
+-- Update
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Additem inputitem ->
+            { model | shoppingList = inputitem :: shoppingList }
+
+
+
+--View
+
+
+view : Model -> Html Msg
+view model =
+    div [] [ text shoppingList ]
