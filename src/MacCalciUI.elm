8e3cbd11e6ab108157c5a9cd456ba9961d60768d
amit-singhs <amitdrive003@gmail.com>
2021-03-17 23:47:09 +0530

    First commit in this branch, ui for calci.

---
+++ src/MacCalciUI.elm
@@ -0,0 +1,52 @@
+module MacCalciUI exposing (..)
+
+import Browser
+import Element exposing (..)
+import Element.Background as Background
+import Element.Border as Border
+import Element.Events as Events
+import Element.Font as Font
+import Element.Input as Input
+import Html exposing (Html, button, del, div, input, p)
+import List exposing (..)
+import Main exposing (Model, init, update, view)
+import String
+
+
+
+--Main
+
+
+main =
+    Browser.sandbox { init = init, view = view, update = update }
+
+
+type alias Model =
+    { theNumber : Int
+    }
+
+
+type Msg
+    = UpdateTheNumber
+
+
+init : Model
+init =
+    { theNumber = 56
+    }
+
+
+
+-- Update
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        UpdateTheNumber ->
+            { model | theNumber = model.theNumber + 1 }
+
+
+view : Model -> Element Msg
+view model =
+    row [] []
