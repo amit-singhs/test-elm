919b20a1f946ca978d142117bef3bcea3bdaabe4
amit-singhs <amitdrive003@gmail.com>
2021-03-20 16:17:52 +0530

    Manually copied from git and committing.

--- src/RashanList.elm
+++ src/RashanList.elm
@@ -1,11 +1,16 @@
 module RashanList exposing (..)
 
 import Browser
-import Html exposing (Html, button, del, div, input, p, text)
-import Html.Attributes exposing (id, placeholder, value)
-import Html.Events exposing (onClick, onInput)
+import Element exposing (..)
+import Element.Background as Background
+import Element.Border as Border
+import Element.Events as Events
+import Element.Font as Font
+import Element.Input as Input
+import Html exposing (Html, button, del, div, input, p)
 import List exposing (..)
 import Main exposing (Model, init, update, view)
+import String
 
 
 
@@ -36,9 +41,10 @@ type alias Model =
 
 init : Model
 init =
-    { shoppingList = []
+    { shoppingList =
+        []
     , newItem = ""
-    , idCounter = 0
+    , idCounter = 1
     }
 
 
@@ -57,11 +63,16 @@ update : Msg -> Model -> Model
 update msg model =
     case msg of
         Additem ->
-            { model
-                | shoppingList = Item model.newItem True model.idCounter :: model.shoppingList
-                , newItem = ""
-                , idCounter = model.idCounter + 1
-            }
+            case String.trim model.newItem of
+                "" ->
+                    { model | newItem = "" }
+
+                s ->
+                    { model
+                        | shoppingList = Item s True model.idCounter :: model.shoppingList
+                        , newItem = ""
+                        , idCounter = model.idCounter + 1
+                    }
 
         UpdateNewItem inpTemp ->
             { model | newItem = inpTemp }
@@ -85,28 +96,138 @@ update msg model =
 --View
 
 
-view : Model -> Html Msg
+bgcolor1 =
+    Background.color <| rgb255 255 55 55
+
+
+bgcolor2 =
+    Background.color <| rgb255 55 55 255
+
+
+
+--view : Model -> Html msg
+
+
 view model =
-    div []
-        [ div []
-            [ input [ placeholder "Enter item", value model.newItem, onInput UpdateNewItem ] []
-            , button [ onClick Additem ] [ text "Add item" ]
-            , div [] <|
-                renderToList <|
-                    sortBy .id model.shoppingList
-            ]
+    let
+        backgroundImagePath =
+            "https://i.ytimg.com/vi/GlNbzAGZC2M/maxresdefault.jpg"
+
+        behindContentBlock =
+            behindContent <|
+                Element.image
+                    [ height fill
+                    , width fill
+                    ]
+                    { src = backgroundImagePath
+                    , description = "A grocery shop image"
+                    }
+    in
+    layout
+        -- [ Background.color <| rgb255 212 244 246
+        [ Background.gradient
+            { angle = 1.5708
+            , steps =
+                [ rgb255 251 171 126
+                , rgb255 247 206 104
+                ]
+            }
+        , width fill
+        , height fill
+        , padding 10
         ]
+    <|
+        row
+            [ width fill
+            ]
+            [ column
+                [ width fill
+                , height fill
+                , padding 5
+                ]
+                [ row
+                    [ Font.color (Element.rgb255 56 125 138)
+                    , Font.size 36
+                    , Font.heavy
+                    , Font.family
+                        [ Font.external
+                            { name = "Libre Franklin"
+                            , url = "https://fonts.googleapis.com/css2?family=Libre+Franklin:wght@800&display=swap"
+                            }
+                        , Font.sansSerif
+                        ]
+                    ]
+                    [ text "Rashan List" ]
+                , row
+                    []
+                    [ Input.text [ Border.rounded 9 ]
+                        { label = Input.labelHidden "Add new item"
+                        , onChange = UpdateNewItem
+                        , placeholder = Just (Input.placeholder [] (text "Add new item"))
+                        , text = model.newItem
+                        }
+                    , row
+                        [ padding 10
+
+                        -- , Background.color <| Element.rgb255 212 244 246
+                        ]
+                        [ Input.button
+                            [ padding 5
+                            , Border.width 2
+                            , Border.rounded 6
+                            , Border.color <| Element.rgb255 110 150 150
+                            , Background.color <| Element.rgb255 180 200 200
+                            , Font.variant Font.smallCaps
+                            , mouseDown
+                                [ Background.color <| Element.rgb255 51 204 204
+                                , Border.color <| Element.rgb255 51 204 204
+                                , Font.color <| Element.rgb255 255 255 255
+                                ]
+                            , mouseOver
+                                [ Background.color <| Element.rgb255 255 255 255
+                                , Border.color <| Element.rgb255 180 180 190
+                                ]
+                            ]
+                            { onPress = Just Additem
+                            , label = text "Add item"
+                            }
+                        ]
+                    ]
+                ]
+            , column
+                [ width fill
+                , Border.rounded 12
+                , Background.color <| rgba255 255 255 255 0.9
+                , padding 5
+                ]
+                (renderToList (sortBy .id model.shoppingList))
+            ]
 
 
-renderToList : List Item -> List (Html Msg)
+renderToList : List Item -> List (Element Msg)
 renderToList xs =
     case xs of
         [] ->
-            []
+            [ row [] [ text "" ] ]
 
         item :: tail ->
             if item.isPurchased == True then
-                p [ onClick (ItemPurchased item) ] [ text item.label ] :: renderToList tail
+                row
+                    [ padding 6
+                    , Font.color (Element.rgb255 30 170 160)
+                    , Font.heavy
+                    , Events.onClick (ItemPurchased item)
+                    ]
+                    [ text (String.fromInt item.id ++ ".) " ++ item.label) ]
+                    :: renderToList tail
 
             else
-                p [ onClick (ItemUnPurchased item) ] [ del [] [ text item.label ] ] :: renderToList tail
+                row
+                    [ padding 6
+                    , Font.color (Element.rgb255 56 125 138)
+                    , Font.heavy
+                    , Font.strike
+                    , Events.onClick (ItemUnPurchased item)
+                    ]
+                    [ text (String.fromInt item.id ++ ".) " ++ item.label) ]
+                    :: renderToList tail
