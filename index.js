a3fd3699caa1e34e981f9daec4af9d6b5ecacccf
amit-singhs <amitdrive003@gmail.com>
2021-06-18 02:15:39 +0530

    Error removed. Working code.

--- index.js
+++ index.js
@@ -6,19 +6,18 @@ let bchjs = new BCHJS() // Defaults to BCHN network.
 var app = Elm.PortsPos.init({
     node: document.getElementById("elm-node")
 });
+
 app.ports.getCashAddress.subscribe(_ => {
     // create mnemonic
     //let mnemonic = bchjs.Mnemonic.generate(128);
     let mnemonic = localStorage.getItem('wallet');
-    // create seed buffer from mnemonic
-    bchjs.Mnemonic.toSeed(mnemonic).then(seedBuffer => {
-        // create HDNode from seed buffer
-        let hdNode = bchjs.HDNode.fromSeed(seedBuffer);
-        // derive hardened child HDNode
-        let account = bchjs.HDNode.deriveHardened(hdNode, 0);
-        let cashAddress = bchjs.HDNode.toCashAddress(account)
+    console.log(mnemonic)
+    restoreWalletFromMnenomic(mnemonic).then(cashAddress => {
         app.ports.cashAddressReceiver.send(cashAddress)
+
     })
+    // create seed buffer from mnemonic
+
 })
 
 app.ports.getBchPrice.subscribe(_ => {
@@ -26,3 +25,17 @@ app.ports.getBchPrice.subscribe(_ => {
         app.ports.bchPriceReceiver.send(current)
     })
 })
+
+let restoreWalletFromMnenomic = mnenomic => {
+    if (mnenomic) {
+        return bchjs.Mnemonic.toSeed(mnemonic).then(seedBuffer => {
+            // create HDNode from seed buffer
+            let hdNode = bchjs.HDNode.fromSeed(seedBuffer);
+            // derive hardened child HDNode
+            let account = bchjs.HDNode.deriveHardened(hdNode, 0);
+            let cashAddress = bchjs.HDNode.toCashAddress(account)
+            return cashAddress
+        })
+    }
+    return Promise.reject("No Wallet found in localStorage")
+}
